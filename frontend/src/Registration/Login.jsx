d9e35675db506caab70f5d109a8df877b5fc8d2d
yarutiun <y.arutiunofff@gmail.com>
2024-10-22 15:38:38 +0200

    setting up a login page

--- frontend/src/Registration/Login.jsx
+++ frontend/src/Registration/Login.jsx
@@ -5,13 +5,19 @@ import { useState } from 'react';
 const Login = () => {
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    console.log('Login:', login);  
+  };
+  
+
   return (
     <div className={style.main}>
         <input type="text" placeholder="Username" value={login} onChange={(e) => setLogin(e.target.value)} className={style.topInput}/>
         <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} className={style.bottomInput}/>
         <div className={style.buttons}>
-            <button className={style.button}>Login</button>
-            <button className={style.register}><Link to='/register'>Register now</Link></button>
+            <button className={style.button} onClick={handleLogin}>Login</button>
+            <button style={{width: '60%', backgroundColor: ""}} id='buttonBot' className={style.button}><Link to='/register'>Register now</Link></button>
         </div>
     </div>
   )
